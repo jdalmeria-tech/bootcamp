bf47444460c7625db6d255217d4fb9ddaefae5bd
jdalmeria-tech <alm24008@byui.edu>
2025-05-08 22:00:47 +0800

    done (:

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ const calculate = {
   "=" : (firstNum, secondNum) => secondNum,
 };
 
+// responsible for using operators
 function useOperator(operator) {
   const currentValue = Number(calculatorDisplay.textContent);
 
@@ -48,10 +49,31 @@ function useOperator(operator) {
   operatorValue = operator;
 }
 
+// responsible for using decimal
+function addDecimal() {
+  if (awaitingNextValue) {
+    return;
+  }
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
 inputBtns.forEach((inputBtn)=> {
   if(inputBtn.classList.length === 0) {
     inputBtn.addEventListener("click", ()=> addNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("operator")) {
     inputBtn.addEventListener("click", ()=> useOperator(inputBtn.value))
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", ()=> addDecimal())
   }
 });
+
+function resetAll() {
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
+  calculatorDisplay.textContent = "0";
+}
+
+clearBtn.addEventListener("click", resetAll);
